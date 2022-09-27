625bf9f04822a63ca0e135401335727b46dcc599
eugene-coding <eugene.code@yandex.ru>
2022-09-28 01:43:34 +0300

    Added UserController

---
+++ API/Controllers/UserController.cs
@@ -0,0 +1,36 @@
+﻿using API.Data;
+using API.DTO;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public sealed class UserController : ControllerBase
+{
+    private readonly Context _context;
+
+    public UserController(Context context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<IEnumerable<User>> GetUsersAsync()
+    {
+        var query = _context.Users
+            .Select(u => new User
+            {
+                FirstName = u.FirstName,
+                LastName = u.LastName
+            });
+
+        var result = await query
+            .AsNoTracking()
+            .ToListAsync();
+
+        return result;
+    }
+}
