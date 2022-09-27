cf37be378a0cb27f2425496824e7d5d72ea0e5c7
eugene-coding <eugene.code@yandex.ru>
2022-09-28 00:59:16 +0300

    Configured DB connection

---
+++ API/Data/Context.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace API.Data;
+
+internal sealed class Context : DbContext
+{
+    public Context(DbContextOptions<Context> options) : base(options)
+    {
+    }
+}
