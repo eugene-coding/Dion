1d3c3507ed9b807b58982b1e9d1e75f961fe040b
Eugene <eugene.code@yandex.ru>
2022-09-28 17:47:14 +0300

    Deleted hosted ASP.NET project from the web project
    The server part of the site has already been created in the form of a separate API project to support not only the site, but also the application, and at the same time be independent of them

--- Web/Program.cs
+++ Web/Program.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
-using Web.Client;
+using Web;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
