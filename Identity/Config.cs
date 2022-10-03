b61f6fe20ed4e3c8d907019761729253db057613
Eugene <eugene.code@yandex.ru>
2022-10-03 16:29:49 +0300

    Added API Scope and Client

--- Identity/Config.cs
+++ Identity/Config.cs
@@ -4,6 +4,8 @@ namespace Identity;
 
 public static class Config
 {
+    private const string _apiScopeName = "api";
+
     public static IEnumerable<IdentityResource> IdentityResources =>
         new IdentityResource[]
         {
@@ -11,10 +13,25 @@ public static class Config
         };
 
     public static IEnumerable<ApiScope> ApiScopes =>
-        new ApiScope[]
-            { };
+        new List<ApiScope>
+        {
+            new(_apiScopeName, "API")
+        };
 
     public static IEnumerable<Client> Clients =>
-        new Client[]
-            { };
+        new List<Client>
+        {
+            new Client
+            {
+                ClientId = "client",
+                AllowedGrantTypes = GrantTypes.ClientCredentials,
+
+                ClientSecrets =
+                {
+                    new Secret("secret".Sha256())
+                },
+
+                AllowedScopes = {_apiScopeName}
+            }
+        };
 }
