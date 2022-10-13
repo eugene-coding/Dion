6ff67c11387a5ebad4f8994a8751f7afd74b7d09
Eugene <eugene.code@yandex.ru>
2022-10-13 12:05:17 +0300

    Deleted unused User DTO

--- Shared/DTO/User.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Shared.DTO;
-
-public sealed class User
-{
-    public string FirstName { get; set; } = string.Empty;
-    public string? LastName { get; set; }
-    public string? Description { get; set; }
-}
