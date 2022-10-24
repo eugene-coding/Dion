df91eb8419b45466156ecc2279d7359e1d95affe
Eugene <eugene.code@yandex.ru>
2022-10-24 12:24:01 +0300

    Refactored Login Backend

---
+++ Identity/Pages/Account/Login/ExternalProvider.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Authentication;
+
+namespace Identity.Pages.Login;
+
+public sealed class ExternalProvider
+{
+    public string DisplayName { get; set; }
+    public string AuthenticationScheme { get; set; }
+}
