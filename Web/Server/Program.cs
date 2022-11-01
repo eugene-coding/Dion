e3ed37718964ec3d6a36c0109c644e721ed7b1b5
Eugene <eugene.code@yandex.ru>
2022-11-01 13:53:55 +0300

    Added spaces in Server/Program.cs

--- Web/Server/Program.cs
+++ Web/Server/Program.cs
@@ -3,23 +3,29 @@ using Duende.IdentityServer;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 using Shared;
+
 internal static class Program
 {
     private static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
         builder.ConfigureServices();
+
         var app = builder.Build();
         app.ConfigurePipeline();
+
         app.Run();
     }
+
     private static void ConfigureServices(this WebApplicationBuilder builder)
     {
         builder.Services.AddControllersWithViews();
         builder.Services.AddRazorPages();
+
         builder.Services.AddBff();
         builder.Services.ConfigureAuthentication();
     }
+
     private static void ConfigurePipeline(this WebApplication app)
     {
         if (app.Environment.IsDevelopment())
@@ -32,20 +38,27 @@ internal static class Program
             // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
             app.UseHsts();
         }
+
         app.UseHttpsRedirection();
+
         app.UseBlazorFrameworkFiles();
         app.UseStaticFiles();
+
         app.UseRouting();
         app.UseAuthentication();
         app.UseBff();
         app.UseAuthorization();
+
         app.MapBffManagementEndpoints();
         app.MapRazorPages().RequireAuthorization();
+
         app.MapControllers()
            .RequireAuthorization()
            .AsBffApiEndpoint();
+
         app.MapFallbackToFile("index.html");
     }
+
     private static void ConfigureAuthentication(this IServiceCollection services)
     {
         services.AddAuthentication(options =>
