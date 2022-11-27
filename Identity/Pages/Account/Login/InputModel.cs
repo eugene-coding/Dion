396d29f5c3fe030f056dd1a61d1360bdf0ce1d4e
eugene-coding <eugene.code@yandex.ru>
2022-11-27 14:12:43 +0300

    Update InputModel.cs

--- Identity/Pages/Account/Login/InputModel.cs
+++ Identity/Pages/Account/Login/InputModel.cs
@@ -1,9 +1,7 @@
-// Copyright (c) Duende Software. All rights reserved.
-// See LICENSE in the project root for license information.
-
 using Microsoft.AspNetCore.Mvc;
 
 using System.ComponentModel.DataAnnotations;
+using System.Net;
 
 namespace Identity.Pages.Login;
 
@@ -11,7 +9,7 @@ public class InputModel
 {
     [Required]
     [Display(Name = nameof(Username))]
-    [PageRemote(ErrorMessage = "Username doesn`t exist", HttpMethod = "post", PageHandler = "ValidateUsername")]
+    [PageRemote(ErrorMessage = "Username doesn`t exist", HttpMethod = WebRequestMethods.Http.Post, PageHandler = "ValidateUsername")]
     public string Username { get; set; }
 
     public string ReturnUrl { get; set; }
