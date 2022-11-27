5f9c705f8fbd26900478c40ba8d0ecb4a5376c7c
eugene-coding <eugene.code@yandex.ru>
2022-11-27 14:13:17 +0300

    Creating second step of the authentication

--- Identity/Pages/Account/Login/Password/Index.cshtml.cs
+++ Identity/Pages/Account/Login/Password/Index.cshtml.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Identity.Pages.Login.Password;
@@ -7,7 +9,26 @@ namespace Identity.Pages.Login.Password;
 [AllowAnonymous]
 public class IndexModel : PageModel
 {
+    [BindProperty]
+    public InputModel Input { get; set; }
+
     public void OnGet()
     {
+        Input = new();
+    }
+
+    public async Task<JsonResult> OnPostValidateUsernameAsync()
+    {
+        var trimmedUsername = Input.Username.Trim();
+
+        var user = await _userManager.FindByNameAsync(trimmedUsername);
+        var valid = user is not null;
+
+        return new JsonResult(valid);
+    }
+
+    public IActionResult OnGetSuccess(string query)
+    {
+        return Redirect("/Account/Login/Password" + query);
     }
 }
