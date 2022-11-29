a3f9ff900d8b484ca0bd8f02903819a7a2ed1d67
Eugene <eugene.code@yandex.ru>
2022-11-29 15:48:12 +0300

    Deleted password page
    It is unsafe to pass username between pages so that we use the login form on the single page uploading data

--- Identity/Pages/Account/Login/Password/InputModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace Identity.Pages.Login.Password;
-
-public class InputModel : Login.InputModel
-{
-    [DataType(DataType.Password)]
-    public string Password { get; set; }
-}
