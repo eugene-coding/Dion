5a1c9e33a3797fc64982721ca06fd74ba9cf7de2
Eugene <eugene.code@yandex.ru>
2022-11-29 16:23:47 +0300

    Revert "Deleted password page"
    This reverts commit a3f9ff900d8b484ca0bd8f02903819a7a2ed1d67.

---
+++ Identity/Pages/Account/Login/Password/InputModel.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Identity.Pages.Login.Password;
+
+public class InputModel : Login.InputModel
+{
+    [DataType(DataType.Password)]
+    public string Password { get; set; }
+}
