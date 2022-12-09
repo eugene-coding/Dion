5ae48647407b76989af2b1fe6b9b5c8795602349
Eugene <eugene.code@yandex.ru>
2022-12-09 16:04:08 +0300

    Added the Prompt component

---
+++ Identity/Pages/Shared/Prompt.razor.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Identity.Pages.Shared;
+
+public sealed partial class Prompt
+{
+    [Parameter]
+    [EditorRequired]
+    public string Text { get; init; } = string.Empty;
+
+    [Parameter]
+    [EditorRequired]
+    public string ButtonText { get; init; } = string.Empty;
+
+    [Parameter]
+    [EditorRequired]
+    public Uri Url { get; init; } = default!;
+}
