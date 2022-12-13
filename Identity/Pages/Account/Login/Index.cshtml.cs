d8a4216ad9d554441650122f01bd686261ddfd56
Eugene <eugene.code@yandex.ru>
2022-12-13 18:03:54 +0300

    Refactored Index Page

--- Identity/Pages/Account/Login/Index.cshtml.cs
+++ Identity/Pages/Account/Login/Index.cshtml.cs
@@ -35,9 +35,19 @@ public sealed class Index : PageModel
         Localizer = localizer;
     }
 
+    /// <summary>Gets or initializes the return URL.</summary>
+    /// <value>
+    /// The URL to which the user will be redirected 
+    /// after the authorization process is completed
+    /// </value>
     [BindProperty(SupportsGet = true)]
     public string ReturnUrl { get; init; }
 
+    /// <summary>Gets or sets the username.</summary>
+    /// <remarks>
+    /// The username stored in the session.
+    /// When setting, the value is trimmed.
+    /// </remarks>
     [Required]
     [BindProperty]
     [Display(Name = nameof(Username))]
@@ -49,34 +59,44 @@ public sealed class Index : PageModel
     public string Username
     {
         get => HttpContext.Session.GetString(SessionKeys.Username);
-        set => HttpContext.Session.SetString(SessionKeys.Username, value);
+        set => HttpContext.Session.SetString(SessionKeys.Username, value.Trim());
     }
 
-    public string SubmitButtonId => "submit";
+    /// <inheritdoc cref="IStringLocalizer" />
     public IStringLocalizer<Index> Localizer { get; private init; }
-    
+
     public async Task OnGetAsync()
     {
-        var context = await _interaction.GetAuthorizationContextAsync(ReturnUrl);
-        
-        if (context?.IdP is not null && await _schemeProvider.GetSchemeAsync(context.IdP) is not null)
+        var hint = await GetLoginHint();
+
+        if (hint is not null)
         {
-            Username = context?.LoginHint;
+           Username = hint;
         }
     }
 
+    /// <summary>Checks if there is an entry with the username entered.</summary>
+    /// <returns>
+    /// Returns the <see cref="Task"/> that contains <see cref="JsonResult"/> 
+    /// with <see langword="true"/> if a record with the entered username is found, 
+    /// otherwise - <see langword="false"/>.
+    /// </returns>
     public async Task<JsonResult> OnPostValidateUsernameAsync()
     {
-        var trimmedUsername = Username.Trim();
+        var user = await _userManager.FindByNameAsync(Username);
 
-        var user = await _userManager.FindByNameAsync(trimmedUsername);
-        var valid = user is not null;
+        return new JsonResult(user is not null);
+    }
 
-        if (valid)
+    private async Task<string> GetLoginHint()
+    {
+        var context = await _interaction.GetAuthorizationContextAsync(ReturnUrl);
+
+        if (context?.IdP is not null && await _schemeProvider.GetSchemeAsync(context.IdP) is not null)
         {
-            Username = trimmedUsername;
+            return context.LoginHint;
         }
 
-        return new JsonResult(valid);
+        return null;
     }
 }
