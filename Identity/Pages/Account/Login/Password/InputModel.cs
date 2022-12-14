ca5a236e221a3faec5f8489a0293f3903cfb7efb
Eugene <eugene.code@yandex.ru>
2022-12-14 18:00:00 +0300

    Create InputModel.cs

---
+++ Identity/Pages/Account/Login/Password/InputModel.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+using System.ComponentModel.DataAnnotations;
+using System.Net;
+
+namespace Identity.Pages.Login.Password;
+
+/// <summary>Stores data obtained using model binding.</summary>
+public sealed class InputModel
+{
+    /// <summary>Gets or sets the user password.</summary>
+    [Required, FromForm]
+    [BindRequired]
+    [DataType(DataType.Password)]
+    [PageRemote(
+        ErrorMessage = "Can`t sign in",
+        HttpMethod = WebRequestMethods.Http.Post,
+        PageHandler = "TryToSignIn")]
+    public string Password { get; set; }
+
+    /// <inheritdoc cref="Login.IndexModel.ReturnUrl"/>
+    [FromQuery]
+    public string ReturnUrl { get; set; }
+}
