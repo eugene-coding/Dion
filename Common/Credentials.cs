adf6458994d8c4995be2dce435f37cd76c1b0230
Eugene <eugene.code@yandex.ru>
2022-12-19 12:07:39 +0300

    Update Credentials.cs

--- Common/Credentials.cs
+++ Common/Credentials.cs
@@ -6,6 +6,6 @@ public static class Credentials
     /// <summary>Credentials of the Client client.</summary>
     public static Credential Client => new("client", "secret");
 
-    public static Credential Web => new("bff", "secret");
     /// <summary>Credentials of the Web client.</summary>
+    public static Credential Web => new("bff", "secret");
 }
