ae14c07189d77cf98dc818c8540fc7095e803e6d
eugene-coding <eugene.code@yandex.ru>
2022-12-30 01:16:36 +0300

    Fixed data type overload

---
+++ Identity/Extensions/Hosting/SerilogExtension.cs
@@ -0,0 +1,20 @@
+﻿using Serilog;
+
+using System.Globalization;
+
+namespace Identity.Extensions.Hosting;
+
+public static class SerilogExtension
+{
+    public static IHostBuilder ConfigureSerilog(this ConfigureHostBuilder host)
+    {
+        const string outputTemplate = "[{Timestamp:HH:mm:ss} {Level}] {SourceContext}{NewLine}{Message:lj}{NewLine}{Exception}{NewLine}";
+
+        return host.UseSerilog((ctx, lc) => lc
+                   .WriteTo.Console(
+                        outputTemplate: outputTemplate,
+                        formatProvider: CultureInfo.CurrentCulture)
+                   .Enrich.FromLogContext()
+                   .ReadFrom.Configuration(ctx.Configuration));
+    }
+}
