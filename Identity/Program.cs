4674a3b63cb126c2d4838e26318ff16600a03960
eugene-coding <eugene.code@yandex.ru>
2023-01-01 21:08:47 +0300

    Update Program.cs

--- Identity/Program.cs
+++ Identity/Program.cs
@@ -93,7 +93,7 @@ internal static class Program
         {
             OnPrepareResponse = context =>
             {
-                context.Context.Response.Headers.Add(HeaderNames.CacheControl, "public,max-age=600");
+                context.Context.Response.Headers.Add(HeaderNames.CacheControl, "public, max-age=600");
                 context.Context.Response.Headers.Remove(HeaderNames.ContentSecurityPolicy);
 
                 context.Context.Response.SetUtf8Charset();
