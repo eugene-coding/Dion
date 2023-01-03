deedae3a018298725a1ee98f9364e4fe902ea9aa
eugene-coding <eugene.code@yandex.ru>
2023-01-03 07:19:33 +0300

    Update Index.cshtml.cs

--- Identity/Pages/Account/Registration/Index.cshtml.cs
+++ Identity/Pages/Account/Registration/Index.cshtml.cs
@@ -38,25 +38,14 @@ public class IndexModel : PageModel
     }
 
     /// <summary>Checks if there is no user with the <see cref="InputModel.Email">email</see> entered.</summary>
-    /// <remarks>
-    /// If a user with the specified <see cref="InputModel.Email">email</see> is found
-    /// and the <see cref="InputModel.Email">email</see> is not confirmed, 
-    /// the user will be redirected to the email confirmation page.
-    /// </remarks>
     /// <returns>
     /// Returns the <see cref="Task"/> containing the <see cref="JsonResult"/> 
     /// with <see langword="true"/> if a user with the entered email is not found, 
     /// otherwise - <see langword="false"/>.
     /// </returns>
-    public async Task<IActionResult> OnPostCheckEmailAsync()
+    public async Task<JsonResult> OnPostCheckEmailAsync()
     {
         var user = await _userManager.FindByEmailAsync(Input.Email);
-
-        if (user is not null && !user.EmailConfirmed)
-        {
-            // Redirect to the email confirmation page
-            return LocalRedirect("~/");
-        }
 
         return new JsonResult(user is null);
     }
