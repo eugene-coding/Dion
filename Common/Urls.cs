11239480e78d513d821a75a02815eda49b4bc746
eugene-coding <eugene.code@yandex.ru>
2023-01-04 20:16:10 +0300

    Update Urls.cs

--- Common/Urls.cs
+++ Common/Urls.cs
@@ -6,8 +6,11 @@ public static class Urls
     private readonly static Uri _base = new("https://localhost");
 
     /// <summary>URL of the Identity project.</summary>
-    public static Uri Identity => new UriBuilder(_base) { Port = 5001 }.Uri;
+    public static Uri Identity { get; } = new UriBuilder(_base) { Port = 5001 }.Uri;
 
     /// <summary>URL of the Web project.</summary>
-    public static Uri Web => new UriBuilder(_base) { Port = 5002 }.Uri;
+    public static Uri Web { get; } = new UriBuilder(_base) { Port = 5002 }.Uri;
+
+    /// <summary>URL of the Web project`s Redirect page.</summary>
+    public static Uri WebRedirect { get; } = new UriBuilder(Web) { Path = "Redirect" }.Uri;
 }
